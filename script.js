a444bd7507984a9e2a9384d609e4d51b36cd9c05
arader21 <austinrader21@gmail.com>
2025-09-13 16:09:53 -0600

    loops added and background fixed

---
+++ script.js
@@ -0,0 +1,17 @@
+function makeGrids(size) {
+  let screen = document.querySelector(".sketch-screen");
+  for (let i = 0; i < size; i++) {
+    let column = document.createElement("div");
+    column.classList.add("column");
+    for (let j = 1; j <= size; j++) {
+      let row = document.createElement("div");
+      row.classList.add("row");
+      row.style.border = "2px solid black";
+      row.innerText = (i * size) + j;
+      column.appendChild(row);
+    }
+    screen.appendChild(column);
+  }
+}
+
+makeGrids(16);
