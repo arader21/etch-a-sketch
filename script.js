5330aa57cff7a446961a810c8dd0e3784e2301de
arader21 <austinrader21@gmail.com>
2025-09-13 17:39:39 -0600

    hover over title added.Css updated

--- script.js
+++ script.js
@@ -13,5 +13,15 @@ function makeGrids(size) {
     screen.appendChild(column);
   }
 }
-
 makeGrids(16);
+//const sketchscreen = docutment.getElementById('.sketch-screen');
+//('.sketch-screen').addEventListener('mouseover', () => {
+ // myElement.style.backgroundColor = "blue"});
+ // {myElement.style.backgroundColor = ""});
+ let divHover = document.querySelector("div");
+  divHover.addEventListener("mouseover", () => {
+    divHover.style.backgroundColor = "red";
+  })
+  divHover.addEventListener("mouseout", () => {
+    divHover.style.backgroundColor = "";
+  })
